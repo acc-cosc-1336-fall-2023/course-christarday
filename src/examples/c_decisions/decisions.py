fa431ebb9a89a302a88b9f4f1857832ca9374599
christarday <137360488+christarday@users.noreply.github.com>
2023-09-14 21:19:47 +0000

    if elif menu example

--- src/examples/c_decisions/decisions.py
+++ src/examples/c_decisions/decisions.py
@@ -20,6 +20,26 @@ def get_letter_grade(grade):
 
     return letter_grade
 
+def display_menu():
+    print("1-Simple if")
+    print("2-if else")
+    print("3-if elif")
+   
+def run_menu():
+    display_menu()
+    option = input("Enter a menu option(1,2, or 3): ")
 
-    return letter_grade    
-    
+    handle_menu_option(option)
+
+def handle_menu_option(option):
+    if(option == "1"):
+       selected_option_1()
+    elif(option =="2"):
+        print("User selected option 2")
+    elif(option == "3"):
+        print("Use selected option 3")
+    else:
+        print("Invalid option")
+
+def selected_option_1():
+    print("User selected option 1")
--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -1,7 +1,3 @@
 import decisions
 
-grade = int(input("Enter a numerical grade: "))
-
-letter_grade = decisions.get_letter_grade(grade)
-
-print("Grade is: " + letter_grade)
+decisions.run_menu()
