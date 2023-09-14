a3b0b8727f16f98f4fd57b1005786fcb2a696ad5
christarday <137360488+christarday@users.noreply.github.com>
2023-09-14 19:54:08 +0000

    if elif example grades

--- src/examples/c_decisions/decisions.py
+++ src/examples/c_decisions/decisions.py
@@ -1,2 +1,25 @@
 def test_config():
     return True
+
+def get_letter_grade(grade):
+    letter_grade = ""
+    
+    if(grade >= 90 and grade <= 100):
+        letter_grade = "A"
+    elif(grade >= 80 and grade < 90 ):
+         letter_grade = "B"
+    elif(grade >= 70 and grade < 80):
+        letter_grade = "C"
+    elif(grade >= 60 and grade < 70):
+        letter_grade = "D"
+    elif(grade >= 0 and grade < 60):
+        letter_grade = "F"
+    else:
+        letter_grade = "Invalid Grade"
+
+
+    return letter_grade
+
+
+    return letter_grade    
+    
--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -1,9 +1,22 @@
 import unittest
 
-from src.examples.c_decisions.decisions import test_config
+from src.examples.c_decisions.decisions import get_letter_grade, test_config
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_get_letter_grade(self): 
+        self.assertEqual(get_letter_grade(105), "Invalid Grade")
+        self.assertEqual(get_letter_grade(100),"A")
+        self.assertEqual(get_letter_grade(95), "A")
+        self.assertEqual(get_letter_grade(90), "A")
+        self.assertEqual(get_letter_grade(85), "B")
+        self.assertEqual(get_letter_grade(75), "C")
+        self.assertEqual(get_letter_grade(65), "D")
+        self.assertEqual(get_letter_grade(55), "F")
+        self.assertEqual(get_letter_grade(-5), "Invalid Grade")
+       
+     
+    
