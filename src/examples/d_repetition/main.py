36aa9e3b1738e1c4c38f944c1d584c88fe96caeb
christarday <137360488+christarday@users.noreply.github.com>
2023-09-19 19:30:17 +0000

    in class examples of forloops and while loops

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,2 +1,8 @@
 import repetition
 
+# repetition.display_numbers(2)
+# repetition.prompt_user()
+# repetition.for_intro_loop()
+repetition.for_intro_loop_strings()
+
+        
--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -1,9 +1,24 @@
 import unittest
 
-from src.examples.d_repetition.repetition import test_config
+from src.examples.d_repetition.repetition import for_sum_of_squares, sum_of_squares, test_config
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_sum_of_squares(self):
+        self.assertEqual(sum_of_squares(-1) , 0)
+        self.assertEqual(sum_of_squares(3) , 14) 
+        self.assertEqual(sum_of_squares(4) , 30) 
+        self.assertEqual(sum_of_squares(5) , 55) 
+    
+    def test_for_sum_of_squares(self):
+        self.assertEqual(for_sum_of_squares(3), 14)
+        self.assertEqual(for_sum_of_squares(4), 30)
+        self.assertEqual(for_sum_of_squares(5), 55)
+
+
+        
+
+        
