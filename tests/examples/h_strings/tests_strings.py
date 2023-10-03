76406b34ed81fdc075d1dc894f25b9d2f6d63140
christarday <137360488+christarday@users.noreply.github.com>
2023-10-03 22:03:06 +0000

    more indexing string examples slicing while loops

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.h_strings.strings import concat_strings, slice_strings, test_config
+from src.examples.h_strings.strings import concat_strings, slice_string, slice_w_step_value, test_config 
 
 class Test_Config(unittest.TestCase):
 
@@ -8,16 +8,19 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(True, test_config())
 
     def test_concat_strings(self):
-        self.assertEqual(concat_strings("abc,", "def"), "abcdef")
+        self.assertEqual(concat_strings("abc", "def"), "abcdef")
 
-
+    
     def test_slice_string(self):
-        name = "Patty Lynne Smith"
-        self.assertEqual(slice_strings(name), "Lynne")
-        self.assertEqual(name[11:], "Smith")
+        name = "Patty Lynn Smith"
+        self.assertEqual(slice_string(name), "Lynn")
+        self.assertEqual(name[11:],"Smith") 
 
     def test_slice_w_step_value(self):
-        letters = "ABCDEFHIJKLMNOPQRSTUVWXYZ"
+        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.assertEqual(slice_w_step_value(letters), "ACEGIKMOQSUWY")
+
+ 
         
 
 
