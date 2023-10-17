04324b92a1062b36e53e58d38710f76c79608067
christarday <137360488+christarday@users.noreply.github.com>
2023-10-17 21:28:46 +0000

    list param no copy created, reference in class example

--- run_tests.py
+++ run_tests.py
@@ -6,7 +6,7 @@ has the test functions
 #from tests.homework.b_in_proc_out import tests_in_proc_out
 # from tests.examples.a_example import tests_devprocess
 #from tests.homework.e_functions import tests_functions
-from tests.homework.h_strings import tests_strings
+from tests.homework.g_lists_and_tuples import tests_lists_and_tuples
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_strings) 
+suite = unittest.TestLoader().loadTestsFromModule(tests_lists_and_tuples) 
 unittest.TextTestRunner(verbosity=2).run(suite)
