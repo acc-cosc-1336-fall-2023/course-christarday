02cc2c8fea25bfaec5328dd99ea6f8c0bf205b20
christarday <137360488+christarday@users.noreply.github.com>
2023-11-04 20:36:17 +0000

    HMWK 8 tests_i_dict_sets

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -37,3 +37,39 @@ class Test_Config(unittest.TestCase):
             [0.1, 0.3, 0.2, 0.0]
         ]
         self.assertEqual(get_p_distance_matrix_for(nested_list), expected_matrix) 
+
+def test_play_both_sports(self):
+    baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+    basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])
+    self.assertEqual(baseball.intersection(basketball), set(['Carmen', 'Alicia']))
+    print("Play both sports: ", baseball.intersection(basketball))
+
+def test_play_either_sport(self):
+    baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+    basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])
+    self.assertEqual(baseball.union(basketball), set(['Jodi', 'Carmen', 'Aida', 'Alicia', 'Eva', 'Sarah']))
+    print("Play either sport: ", baseball.union(basketball))
+
+def test_play_baseball_not_basketball(self):
+    baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+    basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])
+    self.assertEqual(baseball.difference(basketball), set(['Jodi', 'Aida']))
+    print("Play baseball but not basketball: ", baseball.difference(basketball))
+
+def test_play_base_but_not_basket_also_play_basket_not_base(self): 
+     baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+     basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])
+     print("Play Basketball but not Baseball: ", basketball.difference(baseball))
+     self.assertEqual(basketball.difference(baseball), set(['Eva', 'Sarah']))
+
+def test_play_one_sport_but_not_both(self):
+     baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+     basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])
+     self.assertEqual(baseball.symmetric_difference(basketball), set(['Jodi','Aida','Eva','Sarah']))
+     print("Play only one sport: ", baseball.symmetric_difference(basketball))
+
+
+
+
+
+        
